2f272d86c5d9c1a2181bb09cd6e287dad06d01ed
RajdeepSingh14 <rs9746473@gmail.com>
2025-12-24 22:19:03 +0530

    Master Management

--- app/Views/header.php
+++ app/Views/header.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<link rel="icon" type="image/png" href="<?php echo base_url(); ?>assets/img/favicon.png">
     <title><?php echo $title;?></title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
    <link href="<?php echo base_url(); ?>assets/font-awesome/css/font-awesome.css" rel="stylesheet">
--- app/Views/mastermanagement.php
+++ app/Views/mastermanagement.php
@@ -1,64 +1,62 @@
 
 
-<!-- <div class="master"> -->
-    <!-- <div class="card red" ><a href="<?php echo base_url()?>Material">Finish Material</a></div>
-    <div class="card orange" ><a href="<?php echo base_url()?>MRMaterial">MR Material</a></div>
-    <div class="card olive" ><a href="<?php echo base_url()?>FinishStock">Finish Stock</a></div>
-
-    <div class="card blue" ><a href="<?php echo base_url()?>Customer">Customer Master</a></div>
-    <div class="card pink" ><a href="<?php echo base_url()?>CustomerQuota">Customer Quota</a></div>
-    <div class="card teal" ><a href="<?php echo base_url()?>CustomerTransit">Transit</a></div>
-
-    <div class="card yellow" ><a href="<?php echo base_url()?>Machine">Machine</a></div>
-    <div class="card maroon" ><a href="<?php echo base_url()?>MachineAvailability">Machine Availibility</a></div> -->
 <div class="container-fluid py-4">
-    <div class="row g-3">
+    <div class="row g-4">
 
-        <div class="col-lg-4 col-md-6">
+        <div class="col-lg-3 col-md-6">
+            
             <a href="<?php echo base_url()?>Material" class="master-card bg-red">
-                <span>Finish Material</span>
+                <span><i class="fa fa-user pr-2" style="font-size:30px;"></i>  Finish Material
+                </span>
             </a>
         </div>
 
-        <div class="col-lg-4 col-md-6">
-            <a href="<?php echo base_url()?>MRMaterial" class="master-card bg-orange">
-                <span>MR Material</span>
+        <div class="col-lg-3 col-md-6">
+            <a href="<?php echo base_url()?>MRMaterial" class="master-card bg-red">
+                <span><i class="fa fa-user pr-2" style="font-size:30px;"></i>  MR Material
+                </span>
             </a>
         </div>
 
-        <div class="col-lg-4 col-md-6">
-            <a href="<?php echo base_url()?>FinishStock" class="master-card bg-olive">
-                <span>Finish Stock</span>
+        <div class="col-lg-3 col-md-6">
+            <a href="<?php echo base_url()?>FinishStock" class="master-card bg-red">
+                <span><i class="fa fa-user pr-2" style="font-size:30px;"></i>  Finish Stock
+                </span>
             </a>
         </div>
 
-        <div class="col-lg-4 col-md-6">
-            <a href="<?php echo base_url()?>Customer" class="master-card bg-pink">
-                <span>Customer Master</span>
+        <div class="col-lg-3 col-md-6">
+            <a href="<?php echo base_url()?>Customer" class="master-card bg-red">
+                <span><i class="fa fa-user pr-2" style="font-size:30px;"></i>  Customer Master
+                </span>
             </a>
         </div>
 
-        <div class="col-lg-4 col-md-6">
-            <a href="<?php echo base_url()?>CustomerQuota" class="master-card bg-teal">
-                <span>Customer Quota</span>
+        <div class="col-lg-3 col-md-6">
+            <a href="<?php echo base_url()?>CustomerQuota" class="master-card bg-red">
+                <span><i class="fa fa-user pr-2" style="font-size:30px;"></i>  Customer Quota
+                </span>
             </a>
         </div>
 
-        <div class="col-lg-4 col-md-6">
-            <a href="<?php echo base_url()?>CustomerTransit" class="master-card bg-yellow">
-                <span>Customer Transit</span>
+        <div class="col-lg-3 col-md-6">
+            <a href="<?php echo base_url()?>CustomerTransit" class="master-card bg-red">
+                <span><i class="fa fa-truck pr-2" style="font-size:30px;"></i>  Customer Transit
+                </span>
             </a>
         </div>
 
-        <div class="col-lg-4 col-md-6">
-            <a href="<?php echo base_url()?>Machine" class="master-card bg-maroon">
-                <span>Machine</span>
+        <div class="col-lg-3 col-md-6">
+            <a href="<?php echo base_url()?>Machine" class="master-card bg-red">
+                <span><i class="fa fa-machine pr-2" style="font-size:30px;"></i>  Machine
+                </span>
             </a>
         </div>
 
-        <div class="col-lg-4 col-md-6">
-            <a href="<?php echo base_url()?>MachineAvailability" class="master-card bg-orange">
-                <span>Machine Availability</span>
+        <div class="col-lg-3 col-md-6">
+            <a href="<?php echo base_url()?>MachineAvailability" class="master-card bg-red">
+                <span><i class="fa fa-user pr-2" style="font-size:30px;"></i>  Machine Availability
+                </span>
             </a>
         </div>
 
