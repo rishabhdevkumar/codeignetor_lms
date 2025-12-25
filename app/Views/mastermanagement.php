47a5f754dfd327530ddeaed83f9567b85ff93989
RajdeepSingh14 <rs9746473@gmail.com>
2025-12-25 12:08:43 +0530

    Master Icon Added

--- app/Views/mastermanagement.php
+++ app/Views/mastermanagement.php
@@ -6,14 +6,14 @@
         <div class="col-lg-3 col-md-6">
             
             <a href="<?php echo base_url()?>Material" class="master-card bg-red">
-                <span><i class="fa fa-user pr-2" style="font-size:30px;"></i>  Finish Material
+                <span><i class="fa fa-cubes pr-2" style="font-size:30px;"></i>  Finish Material
                 </span>
             </a>
         </div>
 
         <div class="col-lg-3 col-md-6">
             <a href="<?php echo base_url()?>MRMaterial" class="master-card bg-red">
-                <span><i class="fa fa-user pr-2" style="font-size:30px;"></i>  MR Material
+                <span><i class="fa fa-object-group pr-2" style="font-size:30px;"></i>  MR Material
                 </span>
             </a>
         </div>
@@ -27,7 +27,7 @@
 
         <div class="col-lg-3 col-md-6">
             <a href="<?php echo base_url()?>Customer" class="master-card bg-red">
-                <span><i class="fa fa-user pr-2" style="font-size:30px;"></i>  Customer Master
+                <span><i class="fa fa-address-book pr-2" style="font-size:30px;"></i>  Customer Master
                 </span>
             </a>
         </div>
@@ -48,14 +48,14 @@
 
         <div class="col-lg-3 col-md-6">
             <a href="<?php echo base_url()?>Machine" class="master-card bg-red">
-                <span><i class="fa fa-machine pr-2" style="font-size:30px;"></i>  Machine
+                <span><i class="fa fa-cogs pr-2" style="font-size:30px;"></i>  Machine
                 </span>
             </a>
         </div>
 
         <div class="col-lg-3 col-md-6">
             <a href="<?php echo base_url()?>MachineAvailability" class="master-card bg-red">
-                <span><i class="fa fa-user pr-2" style="font-size:30px;"></i>  Machine Availability
+                <span><i class="fa fa-cogs pr-2" style="font-size:30px;"></i>  Machine Availability
                 </span>
             </a>
         </div>
